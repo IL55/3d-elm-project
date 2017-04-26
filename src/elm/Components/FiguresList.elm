1e00ec833b92d0dd405c5d4ab7f1316e7dfa3d40
Igor Lobiv <igor@salsitasoft.com>
2017-04-26 10:15:59 +0200

    fig figure selection

--- src/elm/Components/FiguresList.elm
+++ src/elm/Components/FiguresList.elm
@@ -118,9 +118,7 @@ getNextFigure seed width =
     gen = Random.int 0 (List.length figuresList - 1)
     (index, newSeed) = Random.step gen seed
 
-    indexTest = 6
-
-    newFigure = Maybe.withDefault emptyFigure (List.drop indexTest figuresList |> List.head)
+    newFigure = Maybe.withDefault emptyFigure (List.drop index figuresList |> List.head)
     pos = newFigure.position
     center = newFigure.position.center
 
